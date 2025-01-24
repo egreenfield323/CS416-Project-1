69b3a5f29f1b6cc66562b8a00cb171ffbf03102e
Evan Greenfield <evangreenfield@live.com>
2025-01-24 13:18:56 -0500

    Add getNeighbors and getVirtualPort methods

--- Project/CS416 Project 1/src/ConfigParser.java
+++ Project/CS416 Project 1/src/ConfigParser.java
@@ -1,4 +1,11 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
 
 public class ConfigParser {
 
@@ -8,9 +15,58 @@ public class ConfigParser {
         this.configFile = configFile;
     }
 
-    public static VirtualPort[] getNeighbors() {
-        VirtualPort[] result = new VirtualPort[]{};
+    public VirtualPort[] getNeighbors(String mac) {
+        List<VirtualPort> result = new ArrayList<>();
 
+        Properties prop = new Properties();
+
+
+        try (FileInputStream fis = new FileInputStream(this.configFile)) {
+            prop.load(fis);
+
+            List<String> macs = new ArrayList<>();
+
+            for (Enumeration<?> e = prop.propertyNames(); e.hasMoreElements(); ) {
+                String current_mac = (String) e.nextElement();
+                if (current_mac.startsWith(mac) && !current_mac.equals(mac)) {
+                    macs.add(current_mac.substring(current_mac.indexOf('-') + 1));
+                }
+            }
+
+            for (String current_mac : macs) {
+                String info = prop.getProperty(current_mac);
+                String ip = info.substring(0, info.indexOf(':'));
+                int port = Integer.parseInt(info.substring(info.indexOf(':') + 1));
+                result.add(new VirtualPort(ip, port));
+            }
+
+        } catch (IOException e) {
+            System.err.println("Could not read config file " + e);
+        }
+
+        if (result.size() <= 0) {
+            System.err.println("Could not find any neighbors for MAC address " + mac);
+        }
+
+        return result.toArray(new VirtualPort[0]);
+    }
+
+    public VirtualPort getVirtualPort(String mac) {
+        VirtualPort result = null;
+
+        Properties prop = new Properties();
+
+        try (FileInputStream fis = new FileInputStream(this.configFile)) {
+            prop.load(fis);
+
+            String info = prop.getProperty(mac);
+            String ip = info.substring(0, info.indexOf(':'));
+            int port = Integer.parseInt(info.substring(info.indexOf(':') + 1));
+            result = (new VirtualPort(ip, port));
+
+        } catch (IOException e) {
+            System.err.println("Could not read config file " + e);
+        }
         return result;
     }
 }
