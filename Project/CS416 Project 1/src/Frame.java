4d70f616242c101f7fc340b363015444999ffdd5
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2025-03-04 18:28:30 -0500

    Use VirtualIP in Frame instead of InetAddress, additionally only use VirtualPort for convertion to and from a packet

--- Project/CS416 Project 1/src/Frame.java
+++ Project/CS416 Project 1/src/Frame.java
@@ -6,14 +6,13 @@ import java.util.Arrays;
 
 public class Frame {
     public byte[] data;
-    public InetAddress destIp;
-    public InetAddress sourceIp;
-    public int destPort;
+    public VirtualIP destIp;
+    public VirtualIP sourceIp;
     public String sourceMac;
     public String destMac;
     public String message;
 
-    public Frame(String sMAC, String dMAC, InetAddress sIP, InetAddress dIP, String msg) {
+    public Frame(String sMAC, String dMAC, VirtualIP sIP, VirtualIP dIP, String msg) {
         sourceMac = sMAC;
         destMac = dMAC;
         sourceIp = sIP;
@@ -22,16 +21,11 @@ public class Frame {
     }
 
     // Intended for frames that read from a packet.
-    public Frame() throws UnknownHostException {
-        this("", "", InetAddress.getByName(""), InetAddress.getByName(""), "");
+    public Frame() {
+        this("", "", new VirtualIP("", ""), new VirtualIP("", ""), "");
     }
 
-    public void readPacket(DatagramPacket packet) throws UnknownHostException {
-        // returns array of strings in this order:
-        // source mac, destination mac, message, destination ip, destination port
-        destIp = packet.getAddress();
-        destPort = packet.getPort();
-
+    public VirtualPort readPacket(DatagramPacket packet) {
         data = packet.getData();
         ByteBuffer buffer = ByteBuffer.wrap(data);
 
@@ -48,20 +42,23 @@ public class Frame {
         int sourceIpLength = buffer.getInt();
         byte[] sourceIpBytes = new byte[sourceIpLength];
         buffer.get(sourceIpBytes);
-        sourceIp = InetAddress.getByName(new String(sourceIpBytes));
+        sourceIp = new VirtualIP(new String(sourceIpBytes));
 
         int destIpLength = buffer.getInt();
         byte[] destIpBytes = new byte[destIpLength];
         buffer.get(destIpBytes);
-        destIp = InetAddress.getByName(new String(destIpBytes));
+        destIp = new VirtualIP(new String(destIpBytes));
 
         int messageLength = buffer.getInt();
         byte[] messageBytes = new byte[messageLength];
         buffer.get(messageBytes);
         message = new String(messageBytes);
+
+        // Returning the "port" the packet was received from. (Basically layer 1.)
+        return new VirtualPort(packet.getAddress(), packet.getPort());
     }
 
-    public DatagramPacket writePacket(InetAddress dIP, int dPort) {
+    public DatagramPacket writePacket(VirtualPort addressTo) {
         // payload order is source MAC, destination MAC, source IP, destination IP, message
         ByteBuffer buffer = ByteBuffer.allocate(1024);
         buffer.putInt(sourceMac.getBytes().length);
@@ -76,19 +73,21 @@ public class Frame {
         buffer.put(message.getBytes());
 
         byte[] payload = Arrays.copyOf(buffer.array(), buffer.position());
-        return new DatagramPacket(payload, payload.length, dIP, dPort);
+        return new DatagramPacket(payload, payload.length, addressTo.ip, addressTo.port);
     }
 
     public static void main(String[] args) throws UnknownHostException {
 
-        Frame test = new Frame("7c:7e:eb:c5:e1:57", "97:f9:01:c2:8e:e7", InetAddress.getByName("123.12.123.23"), InetAddress.getByName("192.168.0.225"), "hello");
+        Frame test = new Frame("7c:7e:eb:c5:e1:57", "97:f9:01:c2:8e:e7", new VirtualIP("net1.A"), new VirtualIP("net3.D"), "hello");
+        VirtualPort sentTo = new VirtualPort(InetAddress.getByName("127.0.0.1"), 3000);
 
-        DatagramPacket packet = test.writePacket(test.destIp, 3000);
-        test.readPacket(packet);
+        DatagramPacket packet = test.writePacket(sentTo);
+        test = new Frame(); // Clearing it just so we know it actually loads data from packet.
+        VirtualPort receivedFrom = test.readPacket(packet);
 
         System.out.printf(
-                "Source Mac: %s, Destination Mac: %s, Source IP: %s, Destination IP: %s, Message:\n%s\nVirtual Port: %s:%d\n",
-                test.sourceMac, test.destMac, test.sourceIp, test.destIp, test.message, test.destPort
+                "Source Mac: %s, Destination Mac: %s, Source IP: %s, Destination IP: %s, Message:\n%s\nVirtual Port: %s\n",
+                test.sourceMac, test.destMac, test.sourceIp, test.destIp, test.message, receivedFrom
         );
     }
 }
