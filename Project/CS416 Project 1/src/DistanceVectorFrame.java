26d559d37bf597dc1e479afd75f7b81818eb8833
Evan Greenfield <evangreenfield@live.com>
2025-03-26 13:02:24 -0400

    Create DistanceVectorFrame.java

---
+++ Project/CS416 Project 1/src/DistanceVectorFrame.java
@@ -0,0 +1,49 @@
+
+import java.nio.ByteBuffer;
+
+public class DistanceVectorFrame {
+    private final Frame frame;
+
+    public DistanceVectorFrame(Frame frame) {
+        this.frame = frame;
+    }
+
+    public void addDistanceVector(DistanceVector dv) {
+        String[] knownSubnets = dv.getKnownSubnets();
+
+        int length = 0;
+        for (String subnet : knownSubnets) {
+            length += subnet.getBytes().length;
+        }
+
+        length += Integer.BYTES * 2 * knownSubnets.length;
+
+        ByteBuffer buffer = ByteBuffer.allocate(length);
+
+        for (String subnet : knownSubnets) {
+            buffer.putInt(subnet.getBytes().length);
+            buffer.put(subnet.getBytes());
+            buffer.putInt(dv.getEntry(subnet));
+        }
+
+        this.frame.data = buffer.array();
+    }
+
+    public DistanceVector getDistanceVector() {
+        DistanceVector dv = new DistanceVector();
+
+        ByteBuffer buffer = ByteBuffer.wrap(frame.data);
+
+        while (buffer.hasRemaining()) {
+            int subnetLength = buffer.getInt();
+            byte[] subnetBytes = new byte[subnetLength];
+            buffer.get(subnetBytes);
+            String subnet = new String(subnetBytes);
+            int distance = buffer.getInt();
+
+            dv.addEntry(subnet, distance);
+        }
+
+        return dv;
+    }
+}
